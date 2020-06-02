94a8386c11f799d6ffb769a78ceaca5d068d1526
maksimhristov123 <maxim.1990@abv.bg>
2020-06-03 00:01:20 +0300

    fix candidate profile page

--- candidates/script.js
+++ candidates/script.js
@@ -1,17 +1,12 @@
-$(document).ready(function () {
-    $("#profile_candidate").toggleClass("col-lg-12");
+$(document).ready(function(){
 
-    $(".open_skills_arrow").click(function () {
-        $("#profile_candidate").toggleClass("col-lg-8");
-        $("#skills_candidate").toggleClass("active_skill_tab");
-        $("#exp_candidate").hasClass("active_xp_tab").toggleClass("active_xp_tab");
-        $(this).css("color","green");
+    $(".open_skills_arrow").click(function() {
+        $(".cand_exp").toggleClass("active_open");
+        
     });
 
-    $(".open_exp_arrow").click(function () {
-        $("#profile_candidate").toggleClass("col-lg-8");
-        $("#exp_candidate").toggleClass("active_xp_tab");
-        $("#skill_candidate").hasClass("active_skill_tab").toggleClass("active_skill_tab");
+    $(".open_exp_arrow").click(function() {
+        $(".cand_skills").toggleClass("active_open");
     });
 
     
